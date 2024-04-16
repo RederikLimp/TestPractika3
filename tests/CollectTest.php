ac7b6a7b0b1ed702648d04e593cd6413aa6d70e6
RederikLimp <kirill.alisa.2004@mail.ru>
2024-04-16 15:47:42 +0700

    first commit

---
+++ tests/CollectTest.php
@@ -0,0 +1,63 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class CollectTest extends TestCase {
+
+    public function testCount() {
+        $collect = new \Collect\Collect([13, 17]);
+        $this->assertSame(2, $collect->count());
+    }
+
+    public function testKeysWithEmptyArray() {
+        $collect = new \Collect\Collect([]); 
+        $this->assertEquals([], $collect->keys()->toArray());
+    }
+
+    public function testKeysWithArray() {
+        $data = ['a' => 1, 'b' => 2, 'c' => 3];
+        $collect = new \Collect\Collect($data);
+        $this->assertEquals(['a', 'b', 'c'], $collect->keys()->toArray());
+    }
+
+    public function testValuesWithEmptyArray() {
+        $collect = new \Collect\Collect([]); 
+        $this->assertEquals([], $collect->values()->toArray());
+    }
+
+    public function testValuesWithArray() {
+        $data = ['a' => 1, 'b' => 2, 'c' => 3];
+        $collect = new \Collect\Collect($data); 
+        $this->assertEquals([1, 2, 3], $collect->values()->toArray());
+    }
+
+    public function testGetWithExistingKey() {
+        $data = ['a' => 1, 'b' => 2, 'c' => 3];
+        $collect = new \Collect\Collect($data); 
+        $this->assertEquals(2, $collect->get('b'));
+    }
+
+    
+    public function testExceptWithSingleElement() {
+        $data = ['a' => 1, 'b' => 2, 'c' => 3];
+        $collect = new \Collect\Collect($data);
+        $result = $collect->except('b');
+        $this->assertEquals(['a' => 1, 'c' => 3], $result->toArray());
+    }
+
+    public function testExceptWithMultipleElements() {
+        $data = ['a' => 1, 'b' => 2, 'c' => 3];
+        $collect = new \Collect\Collect($data); 
+        $result = $collect->except('a', 'c');
+        $this->assertEquals(['b' => 2], $result->toArray());
+    }
+    public function testGetWithNonExistingKeyReturnsArray() {
+        $data = ['a' => 1, 'b' => 2, 'c' => 3];
+        $collect = new \Collect\Collect($data);
+        
+        $result = $collect->get('nonexistent_key');
+        
+        $this->assertIsArray($result);
+        $this->assertEquals($data, $result);
+    }
+}
